03201e49cdd9a8e73e32bc7e4dc1e348bd0354fb
nobyn0312 <nobyn0312@gmail.com>
2023-04-09 23:08:52 +0900

    test

---
+++ components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/container.module.css'
+
+const App = () => {
+  return (
+    <div className={styles.container}>
+      <p>App</p>
+    </div>
+  )
+}
+
+export default App
---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/container.module.css'
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+    <p>フッターのテスト</p>
+    </div>
+  )
+}
+
+export default Footer
